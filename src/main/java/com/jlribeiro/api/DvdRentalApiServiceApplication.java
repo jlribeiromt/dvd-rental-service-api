c382fa2cc18724815a9f5f799558e7e8bd9d0197
jlribeiromt <jl.dr@hotmail.com>
2018-11-28 21:48:32 -0200

    Versão inicial do projeto.

---
+++ src/main/java/com/jlribeiro/api/DvdRentalApiServiceApplication.java
@@ -0,0 +1,12 @@
+package com.jlribeiro.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DvdRentalApiServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DvdRentalApiServiceApplication.class, args);
+	}
+}
